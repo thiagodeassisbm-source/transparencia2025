841b88430ffb6a708c617e805167cf6c60b250c4
thiagodeassisbm <thiago.deassisbm@gmail.com>
2026-03-27 12:39:09 -0300

    fix sidebar navigation context for super admin pages

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -51,7 +51,7 @@ function isActive($pageName) {
         </div>
         <nav class="sidebar-nav">
             <?php 
-            $super_pages = ['super_dashboard.php', 'super_logs.php', 'cadastrar_prefeitura.php', 'switch_pref.php', 'alterar_status_pref.php'];
+            $super_pages = ['super_dashboard.php', 'super_logs.php', 'cadastrar_prefeitura.php', 'editar_prefeitura.php', 'gerenciar_prefeituras.php', 'switch_pref.php', 'alterar_status_pref.php'];
             $is_super_context = in_array(basename($_SERVER['PHP_SELF']), $super_pages);
             
             if ($is_super_context && isset($_SESSION['is_superadmin']) && $_SESSION['is_superadmin'] === 1): 
